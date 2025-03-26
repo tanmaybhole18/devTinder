85d4742d17528db8ac9729d6f95e001b3cf88880
Chinmay-369 <chinmaybhole17@gmail.com>
2025-03-26 18:40:12 +0530

    third commit

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require("express")
 
 const app = express()
 
-app.get("/user", (req,res)=>{
+app.get("/user/:userID/:name/:password", (req,res)=>{
+    console.log(req.params)
     res.send("DATA USING GET METHOD")
 })
 
 
-app.post("/user", (req,res)=>{
-    //logic for saving data to db
-    console.log("Data submitted")
-    res.send("DATA USING post METHOD")
-})
+// app.post("/user", (req,res)=>{
+//     //logic for saving data to db
+//     console.log("Data submitted")
+//     res.send("DATA USING post METHOD")
+// })
 
 app.listen(8000, ()=>{
     console.log("Server is running ");
