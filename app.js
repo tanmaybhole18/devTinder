f2fe7e8b8cf568075b49f2409b4ea95bdc645104
Chinmay-369 <chinmaybhole17@gmail.com>
2025-03-26 18:13:58 +0530

    exploring commit

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require("express")
 
 const app = express()
 
-app.use("/test",(req,res)=>{
-    res.send("Hello From the server's test page  ")
+app.get("/user", (req,res)=>{
+    res.send("DATA USING GET METHOD")
 })
+
+
+app.post("/user", (req,res)=>{
+    //logic for saving data to db
+    console.log("Data submitted")
+    res.send("DATA USING post METHOD")
+})
+
 app.listen(8000, ()=>{
     console.log("Server is running ");
     
