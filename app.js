19fd9dd5f5c2dd6dc0a9f23c5e5fa690464ee92d
Chinmay-369 <chinmaybhole17@gmail.com>
2025-03-28 11:30:41 +0530

    c

--- app.js
+++ app.js
@@ -2,17 +2,15 @@ const express = require("express")
 
 const app = express()
 
-app.get("/user/:userID/:name/:password", (req,res)=>{
-    console.log(req.params)
-    res.send("DATA USING GET METHOD")
+app.use("/user", (req,res, next)=>{
+    console.log("First Response")
+    next();
+},
+(req,res)=>{
+    res.send("Hello Again from the server !! ")
 })
 
 
-// app.post("/user", (req,res)=>{
-//     //logic for saving data to db
-//     console.log("Data submitted")
-//     res.send("DATA USING post METHOD")
-// })
 
 app.listen(8000, ()=>{
     console.log("Server is running ");
